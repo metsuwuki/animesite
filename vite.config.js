439a7081e138f6056cc212b2bd32292df4be74f9
Metsuki <varpman3310@gmail.com>
2026-03-04 21:25:24 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/animesite/',
-  plugins: [react()],
-
+  base: '/animesite/', // обязательно для https://metsuwuki.github.io/animesite/
+  plugins: [react()]
 })
