00e2aae470cfe6c8ac9d353e7b16b6fe3007be07
Metsuki <varpman3310@gmail.com>
2026-03-04 21:49:53 +0100

    chore: stop tracking node_modules, dist and vendor caches

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/animesite/', // обязательно для https://metsuwuki.github.io/animesite/
+  base: '/animesite/',
   plugins: [react()]
 })
